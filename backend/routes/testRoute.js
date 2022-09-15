139d105abe88b503b9d54d57ba2a16f64b7d9115
PadamBhardwaj <padambhardwaj100@gmail.com>
2022-09-15 13:26:31 +0530

    send mail debug

--- backend/routes/testRoute.js
+++ backend/routes/testRoute.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const {test} = require("../controllers/testController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
-router.route("/test").get(isAuthenticatedUser, authorizeRoles("admin") ,test)
-router.route("/test/user").get(isAuthenticatedUser,authorizeRoles("user","client", "admin"), test);
-router.route("/test/client").get(isAuthenticatedUser,authorizeRoles("client", "admin"),test);
-router.route("/test/admin").get(isAuthenticatedUser,authorizeRoles("admin"),test)
+router.route("/test").get(isAuthenticatedUser, authorizeRoles("user") ,test)
+// router.route("/test/user").get(isAuthenticatedUser,authorizeRoles("user","client", "admin"), test);
+// router.route("/test/client").get(isAuthenticatedUser,authorizeRoles("client", "admin"),test);
+// router.route("/test/admin").get(isAuthenticatedUser,authorizeRoles("admin"),test)
 
 module.exports = router
--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,16 +3,17 @@ const nodeMailer = require('nodemailer')
 const sendEmail = async (options) => {
 
     // const transporter = nodeMailer.createTransport({
-    //     host:"smtp.gmail.com",
-    //     port:456,  (correction->465)
-    //     service:'gmail',
-    //     auth:{
-    //         user:'upworkharsh1@gmail.com',
-    //         pass:'wYsLyFgUyFv8CTMd@',
-    //     }
+    //     host: "smtp.gmail.com",
+    //     port: 456,  /*(correction -> 465)*/
+    //     service: 'gmail',
+    //     auth: {
+    //         user: 'upworkharsh1@gmail.com',
+    //         pass: 'wYsLyFgUyFv8CTMd@',
+    //     },
     const transporter = nodeMailer.createTransport({
         host: process.env.SMPT_HOST,
         port: process.env.SMPT_PORT,
+        secure: true,
         service: process.env.SMPT_SERVICE,
         auth: {
             user: process.env.SMPT_MAIL,
