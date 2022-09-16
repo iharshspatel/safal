666859725612545eda073ac9b8709ef159de4a81
PadamBhardwaj <padambhardwaj100@gmail.com>
2022-09-16 20:41:41 +0530

    components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,40 @@
-import React,{useEffect} from 'react';
-import {Routes, Route} from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
-import {useDispatch, useSelector} from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { loaduser } from './actions/userAction';
+import Loading from './component/Layout/Loading';
 import Customer from './component/Pages/Customer/Customer';
 import Architect from './component/Pages/Architect/Architect';
 import Dealer from './component/Pages/Dealer/Dealer';
 import Mistry from './component/Pages/Mistry/Mistry';
 import PMC from './component/Pages/PMC/PMC';
+import store from './store';
+import SignIn from './component/Auth/Login/signin';
+import SignUp from './component/Auth/Signup/signup'
 function App() {
-   const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-   const {loading, isAuthenticated, user } = useSelector(state => state.user)
-
+  const { loading, isAuthenticated, user } = useSelector(state => state.user)
 
   useEffect(() => {
-    dispatch(loaduser())
-  },[])
+    store.dispatch(loaduser());
+  }, [])
 
   return (
+    (isAuthenticated && !loading) ? <Customer/>:(
     <>
     <Routes>
-      <Route exact path='/' element={<Customer/>}/>
-      <Route exact path='/architect' element={<Architect/>}/>
-      <Route exact path='/dealer' element={<Dealer/>}/>
-      <Route exact path='/mistry' element={<Mistry/>}/>
-      <Route exact path='/pmc' element={<PMC/>}/>
+      <Route exact path='/' element={ <Customer /> } />
+      <Route exact path='/architect' element={(loading === false && isAuthenticated === true) ? <Architect />:<SignIn/>} />
+      <Route exact path='/dealer' element={(loading === false && isAuthenticated === true) ? <Dealer />:<SignIn/>} />
+      <Route exact path='/mistry' element={(loading === false && isAuthenticated === true) ? <Mistry />:<SignIn/>} />
+      <Route exact path='/pmc' element={(loading === false && isAuthenticated === true) ? <PMC />:<SignIn/>} />
+      <Route exact path='/signin' element={<SignIn />} />
+      <Route exact path='/signup' element={<SignUp />} />
     </Routes>
-    </>
+    </>)
   );
 }
 
---
+++ frontend/src/component/Auth/Signup/signup.js
@@ -0,0 +1,143 @@
+import React, { useEffect } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { clearErrors, register, loaduser } from '../../../actions/userAction'
+import { useDispatch, useSelector } from "react-redux"
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+export default function SignUp({history}) {
+    // let initialValue = {
+        // name:'',
+        // email: '',
+        // password: ''
+    // }
+    // const [val, setVal] = useState(initialValue)
+    const dispatch = useDispatch();
+    const { user, isAuthenticated, error } = useSelector(state => state.user);
+    useEffect(() => {
+        if (error) {
+          dispatch(clearErrors())
+        }
+        if (isAuthenticated) {
+          history.push("/")
+        }
+      }, [isAuthenticated, history,user, error, dispatch, clearErrors])
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const val={
+            name:data.get('name'),
+            email:data.get('email'),
+            password:data.get('password')
+        }
+        dispatch(register(val));
+        console.log(val);
+        // console.log({
+            // name: data.get('name'),
+            // email: data.get('email'),
+            // password: data.get('password'),
+        // });
+    };
+    const classes = useStyles();
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <form className={classes.form} noValidate onSubmit={handleSubmit}>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="name"
+                        label="Name"
+                        name="name"
+                        autoComplete="name"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Sign Up
+                    </Button>
+                </form>
+            </div>
+            <Box mt={8}>
+                <Copyright />
+            </Box>
+        </Container>
+    );
+}
