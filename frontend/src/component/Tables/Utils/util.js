89323da2a895ca0ee9a3fcb0e0edb43ef1b686aa
harsh patel <harsh.1722.patel@gmail.com>
2022-12-17 10:15:33 +0530

    dateformat change

--- frontend/src/component/Tables/Utils/util.js
+++ frontend/src/component/Tables/Utils/util.js
@@ -5,7 +5,7 @@ export const dateformater = (date1) => {
     let year = date.getFullYear();
     let month = (date.getMonth() + 1) > 9 ? date.getMonth() + 1 : '0' + (date.getMonth()+1);
     let day = (date.getDate() ) > 9 ? date.getDate()  : '0' + date.getDate();
-    return `${year}-${month}-${day}`
+    return `${day}-${month}-${year}`
     }
     return null
   }
